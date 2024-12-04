d95c50d5f88126c23ba5f917880e4e68c26cc1df
Nathan McGugan <nathan.mcgugan@gmail.com>
2024-12-03 22:07:33 -0400

    Updated NeuralNetwork.py to support images and finished evaluate.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -13,9 +13,14 @@ class NeuralNetwork:
     A simple neural network that can predict a single float from a multidimensional vector.
     """
 
-    def __init__(self, learning_rate, vector_dimension=2):
-        self.weights = np.array([np.random.randn() for _ in range(vector_dimension)])
-        self.bias = np.random.randn()
+    def __init__(self, learning_rate, input_dimension, output_shape=(3, 3)):
+        self.input_dimension = input_dimension
+        self.output_shape = output_shape
+        self.output_size = np.prod(output_shape)  # Number of neurons in the output layer
+
+        # Initialize weights and biases for the network
+        self.weights = np.random.randn(input_dimension, self.output_size)
+        self.bias = np.random.randn(self.output_size)
         self.learning_rate = learning_rate
 
     def _sigmoid(self, x):
@@ -26,75 +31,62 @@ class NeuralNetwork:
 
     def predict(self, input_vector):
         layer_1 = np.dot(input_vector, self.weights) + self.bias
-        layer_2 = self._sigmoid(layer_1)  # output is 'Prediction'
-        return layer_2, layer_1
+        layer_2 = self._sigmoid(layer_1)
+        return layer_2.reshape(self.output_shape), layer_1
 
-    def _compute_gradients(self, input_vector, target):
+    def _compute_gradients(self, input_vector, target_matrix):
         layer_1 = np.dot(input_vector, self.weights) + self.bias
         layer_2 = self._sigmoid(layer_1)
         prediction = layer_2
 
+        
         """
         In this whole 'derivative block of code' what we really want is the derivative of the error, with respect to 
         the weights. This tells us how the error changes when the weights are changed. It's hard to get an exact 
         derivative for that, so we get a couple derivatives that we can and multiply them together.
 
-        This is the formula we will use:
-        d_error/d_weights = d_error/d_prediction * d_prediction/d_layer1 * d_layer1/d_weights
-
         When multiplying these derivatives, a couple of the variables cancel out.
         This leaves us with the derivative of the error with respect to the weights.
         """
 
+        # Flatten the target matrix for gradient computation
+        target_flat = np.array(target_matrix).flatten()
+        input_vector = np.array(input_vector)
+
         # Derivative of the error with respect to the weights
-        d_error_d_prediction = 2 * (prediction - target)
-
-        # Derivative of the prediction with respect to layer1
+        d_error_d_prediction = 2 * (prediction - target_flat)
         d_prediction_d_layer1 = self._sigmoid_prime(layer_1)
-
-        # The derivative of layer1 with respect to the weights simplifies to the input
-        d_layer1_d_weights = input_vector
-
-        # d_layer1_d_bias is pretty easy to find as it is 1, since everything else in layer1 'disappears' when derived
-        d_layer1_d_bias = 1
-
-        d_error_d_weights = d_error_d_prediction * d_prediction_d_layer1 * d_layer1_d_weights
-
-        # Similar process for the bias, and d_layer1_d_bias is pretty easy to find as it is 1
-        d_error_d_bias = d_error_d_prediction * d_prediction_d_layer1 * d_layer1_d_bias
+        d_layer1_d_weights = input_vector[:, np.newaxis]
+        d_error_d_weights = np.dot(d_layer1_d_weights, (d_error_d_prediction * d_prediction_d_layer1)[np.newaxis, :])
+        d_error_d_bias = d_error_d_prediction * d_prediction_d_layer1
 
         return d_error_d_bias, d_error_d_weights
+
 
     def _update_parameters(self, d_error_d_bias, d_error_d_weights):
         self.bias = self.bias - (self.learning_rate * d_error_d_bias)
         self.weights = self.weights - (self.learning_rate * d_error_d_weights)
 
-    def train(self, input_vectors, targets, iterations):
+    def train(self, input_vectors, target_matrices, iterations):
         cumulative_errors = []
         for current_iteration in range(iterations):
-            # Pick a random piece of data
             random_data_index = np.random.randint(len(input_vectors))
 
             input_vector = input_vectors[random_data_index]
-            target = targets[random_data_index]
+            target_matrix = target_matrices[random_data_index]
 
-            # Compute the gradients and update the weights
-            d_error_d_bias, d_error_d_weights = self._compute_gradients(input_vector, target)
-
+            d_error_d_bias, d_error_d_weights = self._compute_gradients(input_vector, target_matrix)
             self._update_parameters(d_error_d_bias, d_error_d_weights)
 
-            # Measure the error across all test data every 100 iterations
             if current_iteration % 100 == 0:
                 cumulative_error = 0
-
                 for data_instance_index in range(len(input_vectors)):
                     data_point = input_vectors[data_instance_index]
-                    target = targets[data_instance_index]
+                    target_matrix = target_matrices[data_instance_index]
 
                     prediction = self.predict(data_point)[0]
-                    error = np.square(prediction - target)
-
-                    cumulative_error = cumulative_error + error
+                    error = np.sum(np.square(prediction - target_matrix))
+                    cumulative_error += error
 
                 cumulative_errors.append(cumulative_error)
 
--- evaluate.py
+++ evaluate.py
@@ -1,7 +1,82 @@
+"""
+Author: Nathan McGugan
+
+A script to evaluate the performance of the neural network on the tic-tac-toe dataset.
+"""
+
+import numpy as np
+import pandas as pd
 from NeuralNetwork import NeuralNetwork
-import matplotlib.image as img
+import cv2
 
-image_file = "./training/augmented_images/image2.jpg"
-image = img.imread(image_file).flatten()
+DIMENSION = 256
+ITERATIONS = 10000
 
-nn = NeuralNetwork(0.1, vector_dimension=9)
+input_vectors = []
+target_matrices = []
+
+annotations = pd.read_json("./training/augmented_annotations_copy.json").to_dict()
+
+def symbol_to_int(symbol):
+    if symbol == "O":
+        return 0
+    elif symbol == "X":
+        return 1
+    else:
+        return 2
+
+def int_to_symbol(integer):
+    if integer == 0 or integer < 0.5:
+        return "O"
+    elif integer == 1 or ( integer > 0.5 and integer < 1.5 ):
+        return "X"
+    else:
+        return " "
+    
+def predict_and_print(network: NeuralNetwork, vector):
+    predictions = network.predict(vector)[0]
+    print(list(map(int_to_symbol, predictions[0])), '\n', \
+          list(map(int_to_symbol, predictions[1])), '\n', \
+          list(map(int_to_symbol, predictions[2])))
+    
+def get_error(predictions, target):
+    return np.sum(np.square(predictions - target))
+    
+def evaluate(network: NeuralNetwork, input_vectors, target_matrices):
+    """
+    Returns the cumulative error and the average error
+    """
+    cumulative_error = 0
+    for i in range(len(input_vectors)):
+        prediction = network.predict(input_vectors[i])[0]
+        error = get_error(prediction, target_matrices[i])
+        cumulative_error += error
+
+    return cumulative_error, cumulative_error / len(input_vectors)
+
+
+for i in range(len(annotations["image"])):
+    image_file = annotations["image"][i]
+    image = cv2.imread(image_file)
+    input_vector = cv2.resize(image, (DIMENSION, DIMENSION)).flatten() / 255.0
+    input_vectors.append(input_vector.tolist())
+
+    target_matrix_string = annotations["conversations"][i][1]['value']
+    rows = target_matrix_string.split("\n")
+    target_matrix = [[], [], []]
+
+    for i, row in enumerate(rows):
+        row_list = list(row)
+        target_matrix[i].append(symbol_to_int(row_list[0]))
+        target_matrix[i].append(symbol_to_int(row_list[1]))
+        target_matrix[i].append(symbol_to_int(row_list[2]))
+    target_matrices.append(target_matrix)
+
+
+nn = NeuralNetwork(learning_rate=0.01, input_dimension=DIMENSION*DIMENSION*3, output_shape=(3, 3))
+errors = nn.train(input_vectors, target_matrices, iterations=ITERATIONS)
+
+error = evaluate(nn, input_vectors, target_matrices)
+
+print(f'Training iterations: {ITERATIONS}, Image dimension: {DIMENSION}X{DIMENSION}')
+print(f'Cumulative error: {error[0]}, Average error: {error[1]}')
